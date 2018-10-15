5beffaa08d91ae8f520ca7116bab123e695225b9
michchen <michchen58@gmail.com>
2018-10-15 16:44:54 -0700

    Disable when you are the only player

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -36,35 +36,35 @@ $(function () {
   // })
 
   // input submit
-  $('form').submit(function(){
-    if (numUsers <= 1) {
-      // alert('you are the only player. need 2+ to play');
-      // return false;
-    }
-    const curText = validate($('#inputText').val());
-    if (curText) {
-      let myData = {
-        id: gameId,
-        user: curUser,
-        text: curText
-      };
-
-      $.ajax({
-        method: 'POST',
-        url: '/api/post',
-        contentType: 'application/json',
-        data: JSON.stringify(myData)
-      }).done(() => {
-        $('#inputText').val('');
-        console.log(`send message`);
-        socket.emit('message', {
-          user: myData.user,
-          text: myData.text
-        });
-      });
-    }
-    return false; // prevent page refesh
-  }); // end submit cb
+  // $('form').submit(function(){
+  //   if (numUsers <= 1) {
+  //     // alert('you are the only player. need 2+ to play');
+  //     // return false;
+  //   }
+  //   const curText = validate($('#inputText').val());
+  //   if (curText) {
+  //     let myData = {
+  //       id: gameId,
+  //       user: curUser,
+  //       text: curText
+  //     };
+  //
+  //     $.ajax({
+  //       method: 'POST',
+  //       url: '/api/post',
+  //       contentType: 'application/json',
+  //       data: JSON.stringify(myData)
+  //     }).done(() => {
+  //       $('#inputText').val('');
+  //       console.log(`send message`);
+  //       socket.emit('message', {
+  //         user: myData.user,
+  //         text: myData.text
+  //       });
+  //     });
+  //   }
+  //   return false; // prevent page refesh
+  // }); // end submit cb
 
 });
 
@@ -118,8 +118,43 @@ class App extends React.Component {
 
     socket.on('userList', data => {
       app.setState({users: data});
-      // console.log(app.state.users);
     });
+
+    // submit word callback
+
+    $('form').submit(function(){
+      if (Object.entries(app.state.users).length <= 1) {
+        alert('you are the only player. need 2+ to play');
+        return false;
+      }
+      const curText = validate($('#inputText').val());
+      if (curText) {
+        let myData = {
+          id: gameId,
+          user: curUser,
+          text: curText
+        };
+
+        $.ajax({
+          method: 'POST',
+          url: '/api/post',
+          contentType: 'application/json',
+          data: JSON.stringify(myData)
+        }).done(() => {
+          $('#inputText').val('');
+          console.log(`send message`);
+          socket.emit('message', {
+            user: myData.user,
+            text: myData.text
+          });
+        });
+      }
+      return false;
+    }); // end submit cb
+
+
+
+
 
     this.getGame(this, gameId);
   }
